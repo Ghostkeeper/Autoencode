96425310af228ecc0c96eede724c2aa7511ff0b4
Ghostkeeper <rubend@tutanota.com>
2018-04-08 17:36:03 +0200

    Clean up after completion
    This way the input file ends up back where it was when we began. It allows me to run the script continuously without having to rename the input file back every time.

--- encode.py
+++ encode.py
@@ -6,6 +6,7 @@ import shutil #To move files.
 import subprocess #To call the encoders and muxers.
 import uuid #To rename files to something that doesn't exist yet.
 
+
 parser = argparse.ArgumentParser(description="Re-encode videos.")
 parser.add_argument("input_filename", metavar="input", type=str, help="The input file name to encode.")
 parser.add_argument("--preset", dest="preset", type=str, help="Preset for encoding. Must be one of: 'sd', 'hd', 'animated'")
@@ -17,14 +18,27 @@ guid = uuid.uuid4().hex #A new file name that is almost guaranteed to not exist 
 extension = os.path.splitext(input_filename)[1]
 shutil.move(input_filename, guid + extension)
 
-if extension == ".mkv":
-	in_mkv = guid + ".mkv"
-	#Find the tracks and attachments in the MKV file.
+def clean():
+	"""Cleans up the changes we made after everything is done."""
+	try:
+		shutil.move(guid + extension, input_filename)
+	except Exception as e:
+		print(e)
+		pass #Can't recover, but continue with the rest of the clean-up.
+
+def extract_mkv(in_mkv):
+	#Find all tracks and attachments in the MKV file.
 	process = subprocess.Popen(["mkvinfo", in_mkv], stdout=subprocess.PIPE)
 	(cout, cerr) = process.communicate()
 	exit_code = process.wait()
 	if exit_code != 0:
 		raise Exception("Calling MKVInfo failed with exit code {exit_code}. CERR: {cerr}".format(exit_code=exit_code, cerr=cerr))
 	print(cout.decode("utf-8")) #Debug.
-else:
-	raise Exception("Unknown file extension: {extension}".format(extension=extension))
+
+try:
+	if extension == ".mkv":
+		extract_mkv(guid + ".mkv")
+	else:
+		raise Exception("Unknown file extension: {extension}".format(extension=extension))
+finally:
+	clean() #Clean up after any mistakes.
