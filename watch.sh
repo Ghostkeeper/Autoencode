e4950e869028824b479b26661b59d1a2ba1edee6
Ghostkeeper <rubend@tutanota.com>
2018-04-14 19:42:33 +0200

    Don't start processing until file write is complete
    Previously we would start processing when the file had just started copying, resulting in the processing of an incomplete file. This resulted in only the first few seconds being processed successfully, and thus a broken video. We need to wait until the file is completely copied, so wait until close_write instead of create.

--- watch.sh
+++ watch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-inotifywait -e moved_to,create -m --exclude "$1/output" "$1" |
+inotifywait -e moved_to,close_write -m --exclude "$1/output" "$1" |
 while read -r directory events filename; do
 	python3 $(dirname "$0")/encode.py "$directory/$filename" "$1/output/$filename"
 done
