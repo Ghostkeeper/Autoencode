3b47b7805e799e7a6c3436a069e9842e09a98b74
Ghostkeeper <rubend@tutanota.com>
2020-02-14 02:55:49 +0100

    Supply correct input and separate preset
    The preset now has to be separate since the input is an absolute path where we can't detect the preset any more from the path.

--- watch.sh
+++ watch.sh
@@ -3,5 +3,5 @@
 inotifywait -r -e moved_to,close_write -m --exclude "$1/output" --format "%e %w%f" "$1" |
   while read events filepath; do
     name="$(echo $filepath | cut -d'/' -f6-)"
-    python3 $(dirname "$0")/encode.py "$name" "$1/output/$name"
+    python3 $(dirname "$0")/encode.py "$1/$name" "$1/output/$name" --preset "$(echo $name | cut -d'/' -f1)"
   done
